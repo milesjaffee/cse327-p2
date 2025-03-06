a9f7189d05d192cd6903c847edafb4d7a9e2dc33
Miles Jaffee <mej327@lehigh.edu>
2025-03-06 13:38:05 -0500

    finished towers problem

--- ai-planning/EvalPlanTowers.java
+++ ai-planning/EvalPlanTowers.java
@@ -24,33 +24,35 @@ class EvalPlanTowers {
             new ActionSchema("Move", 
             new Variable[]{x, y, z}, //disk x, former location y, new location z
             new Literal[]{new Literal(Disk, x), new Literal(Clear, x), new Literal(Clear, z), new Literal(On, x, y), new Literal(Larger, z, x)},
-            new Atom[]{new Atom(On,x,y), new Atom(Clear, z)},
-            new Atom[]{new Atom(On,x,z), new Atom(Clear, y)});
+            new Atom[]{new Atom(On,x,z), new Atom(Clear, y)},
+            new Atom[]{new Atom(On,x,y), new Atom(Clear, z)}
+            );
 
         final ActionSchema[] towerActions = {move};
 
-        final Atom[] initialState1 = {
+        final Atom[] initialState3 = {
             new Atom(Disk, disk1), new Atom(Disk, disk2), new Atom(Disk, disk3),
-            new Atom(On, disk1, rod1), new Atom(On, disk2, rod1), new Atom(On, disk3, rod1),
-            new Atom(Clear, disk3), new Atom(Clear, disk2),
-            new Atom(Larger, disk2, disk1), new Atom(Larger, disk3, disk1),
+            new Atom(On, disk1, disk2), new Atom(On, disk2, disk3), new Atom(On, disk3, rod1),
+            new Atom(Clear, disk1), new Atom(Clear, rod2), new Atom(Clear, rod3),
+            new Atom(Larger, disk2, disk1), new Atom(Larger, disk3, disk2), new Atom(Larger, disk3, disk1),
             new Atom(Larger, rod1, disk1), new Atom(Larger, rod2, disk1), new Atom(Larger, rod3, disk1),
             new Atom(Larger, rod1, disk2), new Atom(Larger, rod2, disk2), new Atom(Larger, rod3, disk2),
             new Atom(Larger, rod1, disk3), new Atom(Larger, rod2, disk3), new Atom(Larger, rod3, disk3),
-            new Atom(Clear, rod2), new Atom(Clear, rod3),
+            
         };
 
-        final Literal[] goalState1 = {
-            new Literal(On, disk1, rod3), new Literal(On, disk2, rod3), new Literal(On, disk3, rod3),
+        final Literal[] goalState3 = {
+            new Literal(On, disk1, disk2), new Literal(On, disk2, disk3), new Literal(On, disk3, rod3),
+            //new Literal(Clear, disk1), new Literal(Clear, rod1), new Literal(Clear, rod2),
         };
 
-        PlanningProblem towersThreeDisks = new PlanningProblem(initialState1, goalState1, towerActions);
+        PlanningProblem towersThreeDisks = new PlanningProblem(initialState3, goalState3, towerActions);
         System.out.println("Initial State: ");
-        for (Atom a: initialState1) {
+        for (Atom a: initialState3) {
             System.out.println(a);
         }
         System.out.println("Goal State: ");
-        for (Literal l: goalState1) {
+        for (Literal l: goalState3) {
             System.out.println(l);
         }
         System.out.println("Plan: ");
